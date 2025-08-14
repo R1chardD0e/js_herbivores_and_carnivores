12709f15e507e6680de17ec9dc21db6f5ef7d3cc
R1chardD0e <109975760+R1chardD0e@users.noreply.github.com>
2025-08-14 23:47:47 +0000

    Solution2

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -27,18 +27,12 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(target) {
-    if (target instanceof Carnivore) {
-      return;
-    }
+    if (target instanceof Herbivore && !target.hidden) {
+      target.health -= 50;
 
-    if (target.hidden) {
-      return;
-    }
-
-    target.health -= 50;
-
-    if (target.health <= 0) {
-      target.die();
+      if (target.health <= 0) {
+        target.die();
+      }
     }
   }
 }
